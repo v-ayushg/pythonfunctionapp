017a59d4857e41c93781456e223363ebcb51e660
Ayush Gupta <152964099+v-ayushg@users.noreply.github.com>
2025-05-26 17:49:00 +0000

    new timer

---
+++ TimerFunction/__init__.py
@@ -0,0 +1,8 @@
+import datetime
+import logging
+
+import azure.functions as func
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    logging.info(f"Hello World! Timer function ran at {utc_timestamp}")
